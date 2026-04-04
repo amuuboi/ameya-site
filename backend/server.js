ab83b3176031e383e920ffdb6196d3573a3e265e
Ameya Bhat <ameya.bhat15@gmail.com>
2026-04-04 16:10:47 -0600

    Standardizing project: moving backend into repo and adding gitignore

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: "Backend working" });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
