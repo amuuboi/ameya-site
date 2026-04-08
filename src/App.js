28877db9714136488af86a3eb72398edd2f2e263
Ameya_Bhat <105542713+amuuboi@users.noreply.github.com>
2026-04-08 15:04:47 -0600

    Update App.js
    New ui

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import the Navbar (Assuming you create Navbar.js in the src folder)
+import Navbar from './Navbar'; 
+
+// Import your pages from the 'pages' folder!
 import Home from './pages/Home';
 import CV from './pages/CV';
 import Apps from './pages/Apps';
-import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/cv">Curriculum Vitae</Link>
-          <Link to="/apps">Apps</Link>
-        </nav>
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cv" element={<CV />} />
-            <Route path="/apps" element={<Apps />} />
-          </Routes>
-        </main>
-      </div>
+      <Navbar /> 
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cv" element={<CV />} />
+        <Route path="/apps" element={<Apps />} />
+      </Routes>
     </Router>
   );
 }
