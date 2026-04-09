8431bc5d38bb12c78b80760f2d6961a693f9db9b
Ameya_Bhat <105542713+amuuboi@users.noreply.github.com>
2026-04-08 23:24:26 -0600

    Update server.js
    parity

--- backend/server.js
+++ backend/server.js
@@ -64,6 +64,19 @@ app.post('/api/admin/deploy', verifyToken, (req, res) => {
     });
 });
 
+//Emergency Contact
+app.post('/api/emergency-contacts', (req, res) => {
+    const { password } = req.body;
+    // IMPORTANT: Use console.log so we can see it in pm2 logs!
+    console.log("Attempting emergency login with:", password); 
+
+    if (password === process.env.EMERGENCY_PW) {
+        const contacts = JSON.parse(process.env.CONTACT_DATA);
+        return res.json({ success: true, contacts });
+    }
+    res.status(401).json({ success: false, message: "Wrong password" });
+});
+
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
